45d7ec8b2a65c50d5007877cd9911b7ec74a17fa
Daniel Fiala <danfiala23@gmail.com>
2025-03-10 12:05:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# next-intl-bug-repro-app-router
+
+A minimalistic example that uses the Next.js App Router, useful for creating bug reproductions.
+
+[Demo](https://next-intl-bug-repro-app-router.vercel.app)
---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,37 @@
+import {routing} from '@/i18n/routing';
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {ReactNode} from 'react';
+
+type Props = {
+  children: ReactNode;
+  params: {locale: string};
+};
+
+export default async function LocaleLayout({
+  children,
+  params: {locale}
+}: Props) {
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <head>
+        <title>next-intl-bug-repro-app-router</title>
+      </head>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
